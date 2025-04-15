3634468508ae59a1fcd807059738131c48443493
Pauljnr <paul.ncube14@gmail.com>
2025-04-15 17:37:01 +0200

    Added files: index.html, app.js, styles.css, and folder: assets with imgs

---
+++ app.js
@@ -0,0 +1,78 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  // second click
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+   updateScore();
+   resetBoard();
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+// make scoreboard fucntional
+const scoreboard = document.querySelector('.scoreboard');
+const score = document.querySelector('.score');
+let scoreCount = 0;
+const scoreDisplay = document.createElement('div');
+scoreDisplay.classList.add('score-display');
+scoreDisplay.innerText = `Score: ${scoreCount}`;
+scoreboard.appendChild(scoreDisplay);
+function updateScore() {
+  scoreCount++;
+  scoreDisplay.innerText = `Score: ${scoreCount}`;
+}
